e76bbd42859119c71a6be74095a8365a40eac01a
Scott Delbecq <scott.delbecq91@gmail.com>
2026-06-22 18:54:34 +0200

    Fix trailing whitespace in the nose CoG comment

--- src/fastuav/models/stability/static_longitudinal/center_of_gravity/components/cog_airframe.py
+++ src/fastuav/models/stability/static_longitudinal/center_of_gravity/components/cog_airframe.py
@@ -135,7 +135,7 @@ class CoG_fuselage(om.ExplicitComponent):
         m_rear = inputs["data:weight:airframe:fuselage:mass:rear"]
 
         # Nose CG, measured from the tip (x=0). The nose skin is a thin shell
-        # (mass = surface x density), modelled as a prolate half-ellipsoid. 0.55 is representative 
+        # (mass = surface x density), modelled as a prolate half-ellipsoid. 0.55 is representative
         # of a typical elongated nose (l_nose / r_base ~ 2-3).
         x_cg_nose = l_nose * 0.55  # [m]
         x_cg_mid = l_nose + l_mid / 2  # [m]
